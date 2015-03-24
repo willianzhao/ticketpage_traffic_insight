0ecbc17004a0b0161d81bfb31b1bbb73859342d9
willianzhao <willianzhao@gmail.com>
2015-03-24 15:34:28 +0800

    Setup the spout

--- project/src/main/java/org/willian/data/pvinsight/storm/pvInsightTopology.java
+++ project/src/main/java/org/willian/data/pvinsight/storm/pvInsightTopology.java
@@ -1,5 +1,15 @@
 package org.willian.data.pvinsight.storm;
 
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.willian.data.pvinsight.storm.spout.RequestHandleSpout;
+import org.willian.data.pvinsight.storm.spout.RequestScheme;
+import storm.kafka.BrokerHosts;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
+
 import java.io.IOException;
 
 /**
@@ -7,11 +17,36 @@ import java.io.IOException;
  */
 public class PVInsightTopology extends BaseAnalysisTopology {
 
+    private static final Logger logger = LogManager.getLogger(PVInsightTopology.class);
+
     public PVInsightTopology(String configurationFileLocation) throws IOException {
         super(configurationFileLocation);
     }
 
-    public static void main(String[] args){
+
+    private SpoutConfig configureKafkaSpout() {
+        BrokerHosts hosts = new ZkHosts(topologyProperties.getProperty("kafka.zookeeper.host"));
+        String topic = topologyProperties.getProperty("kafka.input.topic");
+        String zkRoot = topologyProperties.getProperty("kafka.input.zkRoot");
+        String consumerGroupId = "RequestInboundSpout";
+        SpoutConfig spoutConfig = new SpoutConfig(hosts, topic, zkRoot, consumerGroupId);
+
+        spoutConfig.scheme = new SchemeAsMultiScheme(new RequestScheme());
+
+        logger.debug("Initialize the kafka spout configure");
+        return spoutConfig;
+    }
+
+    public void setupRequestHandleSpout(TopologyBuilder builder) {
+
+        RequestHandleSpout rhSpout = new RequestHandleSpout(configureKafkaSpout());
+        String rhSpoutID = topologyProperties.getProperty("storm.RQHANDLE_SPOUT_ID");
+        int tasks = Integer.parseInt(topologyProperties.getProperty("storm.RQHANDLE_SPOUT_TASKS"));
+        builder.setSpout(rhSpoutID, rhSpout, tasks);
+        logger.info("Set the incoming request handle spout on Kafka");
+    }
+
+    public static void main(String[] args) {
 
     }
 }
--- project/src/main/java/org/willian/data/pvinsight/storm/spout/RequestHandleSpout.java
+++ project/src/main/java/org/willian/data/pvinsight/storm/spout/RequestHandleSpout.java
@@ -1,43 +1,14 @@
 package org.willian.data.pvinsight.storm.spout;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-
-import java.util.Map;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
 
 /**
  * Created by weilzhao on 3/15/15.
  */
-public class RequestHandleSpout extends BaseRichSpout {
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+public class RequestHandleSpout extends KafkaSpout {
 
-    }
-
-    @Override
-    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
-
-    }
-
-    @Override
-    public void close() {
-
-    }
-
-    @Override
-    public void nextTuple() {
-
-    }
-
-    @Override
-    public void ack(Object o) {
-
-    }
-
-    @Override
-    public void fail(Object o) {
-
+    public RequestHandleSpout(SpoutConfig spoutConf) {
+        super(spoutConf);
     }
 }
